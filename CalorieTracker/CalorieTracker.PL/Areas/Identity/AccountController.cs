ab0106ee2e69d85bfd6c626ea6c2ae7d4c777168
Your Name <you@example.com>
2026-03-14 14:23:01 +0200

    3-Tier Architecture

---
+++ CalorieTracker/CalorieTracker.PL/Areas/Identity/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Core;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.SqlServer.Server;
+
+namespace CalorieTracker.PL.Areas.Identity
+{
+    [Route("api/auth/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+    }
+}
