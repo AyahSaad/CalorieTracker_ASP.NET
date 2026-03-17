1f4461e69aa3a98c1433bd94d6d69d15a208f7fc
Your Name <you@example.com>
2026-03-17 13:06:44 +0200

    edit register request

--- CalorieTracker/CalorieTracker.DAL/DTO/Request/RegisterRequest.cs
+++ CalorieTracker/CalorieTracker.DAL/DTO/Request/RegisterRequest.cs
@@ -13,6 +13,10 @@ namespace CalorieTracker.DAL.DTO.Request
         public string Password { get; set; }
         public string Email { get; set; }
         public string PhoneNumber  { get; set; }
-
+        public float DailyCalorieGoal { get; set; } = 2000;
+        public float? CurrentWeight { get; set; }
+        public float? Height { get; set; }
+        public int? Age { get; set; }
+        public string? Gender { get; set; }
     }
 }
